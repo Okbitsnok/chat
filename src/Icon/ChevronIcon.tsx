b9bc4622299b58e949add1a43c33c6af61836637
AnastasiyaPo <nace.poteryaeva@gmail.com>
2022-06-01 12:13:18 +0700

    feat: add Icons
    (cherry picked from commit 13386f83ebcff50f9a107face2e4fc9484bc0b56)

---
+++ src/Icon/ChevronIcon.tsx
@@ -0,0 +1,14 @@
+import {SvgStyled} from './Icons.styles';
+
+export const ChevronIcon = () => {
+  return (
+    <SvgStyled
+      width="12"
+      height="7"
+      viewBox="0 0 12 7"
+      stroke="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 0.910156L6 5.91016L11 0.910156" />
+    </SvgStyled>
+  );
+};

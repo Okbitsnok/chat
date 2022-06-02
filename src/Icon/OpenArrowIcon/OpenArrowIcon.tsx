616413925d79f5fc914e63c0b8c2f208e8b64f6c
AnastasiyaPo <nace.poteryaeva@gmail.com>
2022-06-02 13:22:49 +0700

    "first" special folder

---
+++ src/Icon/OpenArrowIcon/OpenArrowIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OpenArrowIcon = () => {
+    return (
+        <svg width="8" height="5" viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4 -3.49691e-07L8 5L0 5L4 -3.49691e-07Z" fill="#FF6600"/>
+        </svg>
+
+    );
+};
+
+export default OpenArrowIcon;
